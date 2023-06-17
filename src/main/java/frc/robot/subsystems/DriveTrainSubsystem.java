17ab985cc1d1fe8a132448e71145dc8ecf4426cf
MichaelMoralesReyes <rustichelmet83@gmail.com>
2023-06-16 20:43:01 -0400

    Simple Comment, Will Work On Subsystems Tomorrow

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -2,11 +2,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-<<<<<<< HEAD
 public class DriveTrainSubsystem extends SubsystemBase { 
     // Hello World
-=======
-public class DriveTrainSubsystem extends SubsystemBase{ 
-    // hi
->>>>>>> master
 }
