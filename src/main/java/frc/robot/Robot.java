e8a522b8dd1be6557b7765126e349dd2a94fdc4f
MichaelMoralesReyes <rustichelmet83@gmail.com>
2023-06-20 21:52:28 -0400

    Removed a useless thing from Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,8 +20,6 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
-  private OI operatorInterface;
-
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
