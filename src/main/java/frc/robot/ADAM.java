b4825be0f554a77c203f6cc2cf0d6c564f7ba797
MichaelMoralesReyes <rustichelmet83@gmail.com>
2023-07-30 11:11:41 -0400

    'ADAM' basically finished in terms of functionality, Progress: 90%ish, All that remains is to follow the 'TODO:' Comment

--- src/main/java/frc/robot/ADAM.java
+++ src/main/java/frc/robot/ADAM.java
@@ -1,40 +1,56 @@
+package frc.robot;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import edu.wpi.first.wpilibj.DriverStation;
 
+// Genesis 2:7 - And the Lord God formed man of the dust of the ground, and breathed into his nostrils the breath of life; and man became a living soul.
+// TODO: LOG FILTERING, DETAILED ERROR INFO, CONFIGURABLE PARAMETERS, DOCUMENTATION (LATER), CUSTOM EXCEPTIONS, MORE ERROR CODES, CONFIGURING BASED ON THAT (NOW) 
 public class ADAM implements Thread.UncaughtExceptionHandler {
-    
+
     // Define an enum for error codes
     public enum ADAMErrorCodes {
-        UNKNOWN(0),
-        CUSTOM_ERROR(1),
-        SUBSYSTEM_1_ERROR(2),
-        SUBSYSTEM_2_ERROR(3),
+        OFF(0, "No logging"),
+        FATAL(100, "The application is unusable. Action needs to be taken immediately."),
+        ERROR(200, "An error occurred in the application."),
+        WARN(300, "Something unexpected—though not necessarily an error—happened and needs to be watched."),
+        INFO(400, "A normal, expected, relevant event happened."),
+        DEBUG(500, "Used for debugging purposes"),
+        TRACE(600, "Used for debugging purposes—includes the most detailed information")
         // Add more error codes as needed
         ;
 
         private final int code;
+        private final String message;
 
-        ADAMErrorCodes(int code) {
+        ADAMErrorCodes(int code, String message) {
             this.code = code;
+            this.message = message;
         }
 
         public int getCode() {
             return code;
         }
+
+        public String getMessage() {
+            return message;
+        }
     }
 
     // Define an enum for exception severity levels
     public enum ADAMSeverity {
+        ALL(Level.ALL),
+        CONFIG(Level.CONFIG),
+        FINE(Level.FINE),
+        FINER(Level.FINER),
+        FINEST(Level.FINEST),
         INFO(Level.INFO),
-        WARNING(Level.WARNING),
-        ERROR(Level.SEVERE),
-        // Add more severity levels as needed
-        ;
+        OFF(Level.OFF),
+        SEVERE(Level.SEVERE),
+        WARNING(Level.WARNING);
 
         private final Level level;
 
@@ -49,12 +65,13 @@ public class ADAM implements Thread.UncaughtExceptionHandler {
 
     private static ADAMSeverity getSeverityForError(Throwable e) {
         // Map each exception to its corresponding severity level
-        if (e instanceof Subsystem1Exception) {
-            return ADAMSeverity.ERROR;
-        } else if (e instanceof Subsystem2Exception) {
-            return ADAMSeverity.ERROR;
+        if (e instanceof Exception) {
+            return ADAMSeverity.SEVERE;
+        } else if (e instanceof Exception) {
+            return ADAMSeverity.SEVERE;
         }
-        // Add more cases for other custom exceptions and their severity levels as needed
+        // Add more cases for other custom exceptions and their severity levels as
+        // needed
         else {
             return ADAMSeverity.WARNING; // Default severity level
         }
@@ -70,35 +87,24 @@ public class ADAM implements Thread.UncaughtExceptionHandler {
     private static final long TIME_WINDOW_MS = 10000; // Time window in milliseconds (e.g., 10 seconds)
     private long lastErrorTime = 0;
 
-    // Constructor to accept custom error message, error code, and logging level
-    public ADAM(String customErrorMessage, ADAMErrorCodes errorCode, Level loggingLevel) {
-        this.customErrorMessage = customErrorMessage;
-        this.errorCode = errorCode;
-        logger.setLevel(loggingLevel);
-    }
-
-    // Default constructor with unknown error code and default logging level
-    public ADAM(String customErrorMessage) {
-        this(customErrorMessage, ADAMErrorCodes.UNKNOWN, Level.WARNING);
-    }
-
-    // Constructor to automatically assign error code and logging level based on custom exceptions
-    public ADAM(String customErrorMessage, Throwable e) {
-        this.customErrorMessage = customErrorMessage;
+    // Constructor to automatically assign error code and logging level based on
+    // custom exceptions
+    public ADAM(Throwable e) {
+        this.customErrorMessage = determineErrorCode(e).getMessage();
         this.errorCode = determineErrorCode(e);
         logger.setLevel(getSeverityForError(e).getLevel());
     }
 
     // Method to determine the error code based on the type of exception
     private ADAMErrorCodes determineErrorCode(Throwable e) {
-        if (e instanceof Subsystem1Exception) {
-            return ADAMErrorCodes.SUBSYSTEM_1_ERROR;
-        } else if (e instanceof Subsystem2Exception) {
-            return ADAMErrorCodes.SUBSYSTEM_2_ERROR;
+        if (e instanceof Exception) {
+            return ADAMErrorCodes.ERROR;
+        } else if (e instanceof NullPointerException) {
+            return ADAMErrorCodes.FATAL;
         }
         // Add more cases for other custom exceptions as needed
         else {
-            return ADAMErrorCodes.UNKNOWN;
+            return ADAMErrorCodes.OFF;
         }
     }
 
@@ -127,7 +133,8 @@ public class ADAM implements Thread.UncaughtExceptionHandler {
             errorCounts.put(errorCode, count + 1);
             return true;
         } else {
-            // Don't log the error if the error count exceeds the limit within the time window
+            // Don't log the error if the error count exceeds the limit within the time
+            // window
             return false;
         }
     }
@@ -137,13 +144,24 @@ public class ADAM implements Thread.UncaughtExceptionHandler {
         Date timestamp = new Date();
 
         // Log error with the specified logging level
-        logger.log(logger.getLevel(), "[" + timestamp + "] Uncaught exception (Error Code " + errorCode.getCode() + "): " + customErrorMessage, e);
-        
+        logger.log(logger.getLevel(), "[" + timestamp + "] Uncaught exception (Error Code " + errorCode.getCode()
+                + "): " + customErrorMessage, e);
+
+        // Log class and method where the error occurred
+        StackTraceElement[] stackTrace = e.getStackTrace();
+        if (stackTrace.length > 0) {
+            StackTraceElement topFrame = stackTrace[0];
+            logger.log(logger.getLevel(),
+                    "Error occurred in class: " + topFrame.getClassName() + ", method: " + topFrame.getMethodName());
+        }
+
         // Log error to driver station with custom message and error code
-        DriverStation.reportError("[" + timestamp + "] Uncaught exception (Error Code " + errorCode.getCode() + "): " + customErrorMessage, false);
+        DriverStation.reportError("[" + timestamp + "] Uncaught exception (Error Code " + errorCode.getCode() + "): "
+                + customErrorMessage, false);
 
         // Log error to console with custom message and error code
-        System.err.println("[" + timestamp + "] Uncaught exception (Error Code " + errorCode.getCode() + "): " + customErrorMessage);
+        System.err.println("[" + timestamp + "] Uncaught exception (Error Code " + errorCode.getCode() + "): "
+                + customErrorMessage);
         e.printStackTrace(System.err);
     }
 }
