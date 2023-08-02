4c8ef44a481354475d9cb15144579d887c30866c
MichaelMoralesReyes <rustichelmet83@gmail.com>
2023-08-01 21:19:51 -0400

    Reformatted Constants using Prettier

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,11 +8,15 @@ import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.XboxController;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -90,7 +94,7 @@ public final class Constants {
     public static final int OPERATOR_LOGITECH_BUTTON_RIGHT_STICK = 10;
     public static final int OPERATOR_LOGITECH_BUTTON_LEFT_ANALOG_PUSH = 11;
     public static final int OPERATOR_LOGITECH_BUTTON_RIGHT_ANALOG_PUSH = 12;
-    
+
     public static final int OPERATOR_LOGITECH_AXIS_LEFT_X = 0;
     public static final int OPERATOR_LOGITECH_AXIS_LEFT_Y = 1;
     public static final int OPERATOR_LOGITECH_AXIS_LEFT_TRIGGER = 2;
@@ -140,7 +144,6 @@ public final class Constants {
     public static final PS4Controller.Button DRIVER_PS4_BUTTON_TOUCHPAD = PS4Controller.Button.kTouchpad;
     public static final PS4Controller.Button DRIVER_PS4_BUTTON_TRIANGLE = PS4Controller.Button.kTriangle;
 
-
     // * Constants for Logitech driver controller buttons and axes.
     // * Please refer to the controller's documentation to verify the mappings.
 
@@ -162,9 +165,7 @@ public final class Constants {
     public static final int DRIVER_LOGITECH_AXIS_LEFT_TRIGGER = 2;
     public static final int DRIVER_LOGITEC_AXIS_RIGHT_TRIGGER = 3;
     public static final int DRIVER_LOGITECH_AXIS_RIGHT_X = 4;
-     public static final int DRIVER_LOGITECH_AXIS_RIGHT_Y = 5;
-  } 
-  
-}
+    public static final int DRIVER_LOGITECH_AXIS_RIGHT_Y = 5;
+  }
 
-  
+}
