a396a7743fda53c0f9c9dde61b9b4b09620bb25d
Jacob Lee <jelee231@gmail.com>
2024-01-09 17:17:41 -0500

    Merged KitBot code from https://www.firstinspires.org/resource-library/frc/kitbot.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,19 @@ import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.WestCoastDrive;
 import lombok.Getter;
 
+// From KitBot Code
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.Constants.LauncherConstants;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autos;
+import frc.robot.commands.LaunchNote;
+import frc.robot.commands.PrepareLaunch;
+import frc.robot.subsystems.PWMDrivetrain;
+import frc.robot.subsystems.PWMLauncher;
+
+// import frc.robot.subsystems.CANDrivetrain;
+// import frc.robot.subsystems.CANLauncher;
+
 /** 
  * This class is where the bulk of the robot should be declared. 
  * Since Command-based is a "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot} periodic methods (other than the scheduler calls). 
@@ -29,6 +42,13 @@ import lombok.Getter;
   private @Getter final ExampleCommand exampleCommand = new ExampleCommand(exampleSubsystem);
   private @Getter final XboxController xboxController = new XboxController(RobotMap.DriverConstants.D_XBOX_PORT);
   public @Getter final static Joystick logitech = new Joystick(RobotMap.DriverConstants.D_LOGITECH_PORT);
+
+  // From KitBot
+  private final PWMDrivetrain m_drivetrain = new PWMDrivetrain();
+  private final PWMLauncher m_launcher = new PWMLauncher();
+
+  private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController m_operatorController = new CommandXboxController(OperatorConstants.kOperatorControllerPort);
   
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -42,7 +62,27 @@ import lombok.Getter;
    * Triggers can be created via the {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary predicate, or via the named factories in {@link edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight joysticks}.
    */
   private void configureBindings() {
-    // Add code here
+    // Set the default command for the drivetrain to drive using the joysticks
+    m_drivetrain.setDefaultCommand(
+        new RunCommand(
+            () ->
+                m_drivetrain.arcadeDrive(
+                    -m_driverController.getLeftY(), -m_driverController.getRightX()),
+            m_drivetrain));
+
+    /*Create an inline sequence to run when the operator presses and holds the A (green) button. Run the PrepareLaunch
+     * command for 1 seconds and then run the LaunchNote command */
+    m_operatorController
+        .a()
+        .whileTrue(
+            new PrepareLaunch(m_launcher)
+                .withTimeout(LauncherConstants.kLauncherDelay)
+                .andThen(new LaunchNote(m_launcher))
+                .handleInterrupt(() -> m_launcher.stop()));
+
+    // Set up a binding to run the intake command while the operator is pressing and holding the
+    // left Bumper
+    m_operatorController.leftBumper().whileTrue(m_launcher.getIntakeCommand());
 
     if(logitech.getRawButton(1)) {
       driveCommand.schedule();
@@ -59,6 +99,6 @@ import lombok.Getter;
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return exampleCommand; // basically a placeholder
+    return Autos.exampleAuto(m_drivetrain);
   }
 }
