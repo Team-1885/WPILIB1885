f3880b6b254375f270bd445e972462659069ce96
Michael Morales Reyes <michaelamoralesreyes@gmail.com>
2024-01-09 16:19:49 -0500

    Transfer code again

--- src/main/java/frc/robot/hardware/CTREPigeon.java
+++ src/main/java/frc/robot/hardware/CTREPigeon.java
@@ -1,5 +1,20 @@
 package frc.robot.hardware;
 
+import com.flybotix.hfr.codex.CodexOf;
+
 public class CTREPigeon {
   
+  public enum EPigeon implements CodexOf<Double> {
+    YAW,
+    FUSED_HEADING,
+    ROLL,
+    PITCH,
+    ACCEL_X,
+    ACCEL_Y,
+    JERK_X,
+    JERK_Y,
+    COLLISION
+  }
+
+  public static void map(Codex<Double, EPigeon> pCodex)
 }
